68d21ca28f0aafbff12e20a7c47961e484dc6be7
RomanSapunGit <sapunroma23@gmail.com>
2025-09-21 18:29:33 +0200

    feat(borrowing serializers): add BorrowingDetailSerializer
    Extended BorrowingSerializer with BorrowingDetailSerializer:
    - Serializes book using `BookDetailSerializer`
    - Serializes user using `UserSerializer`
    - Preserves nested payments and read-only fields from base serializer
    - Enables detailed representation of borrowings for API responses

--- borrowing/serializers.py
+++ borrowing/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 
 from borrowing.models import Borrowing
+from library.serializers import BookDetailSerializer
 from payment.serializers import PaymentSerializer
+from user.serializers import UserSerializer
 
 
 class BorrowingSerializer(serializers.ModelSerializer):
@@ -17,3 +19,7 @@ class BorrowingReturnSerializer(BorrowingSerializer):
     class Meta:
         model = Borrowing
         fields = BorrowingSerializer.Meta.fields + ("actual_return_date",)
+
+class BorrowingDetailSerializer(BorrowingSerializer):
+    book = BookDetailSerializer()
+    user = UserSerializer()
