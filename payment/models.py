1683f092b9ae2fba5f615dce7556e62e48188bd4
RomanSapunGit <sapunroma23@gmail.com>
2025-09-21 20:24:13 +0200

    feat(payment models): add Payment model
    - Defined Payment model with fields:
      - status (Pending or Paid)
      - type (Payment or Fine)
      - borrowing (ForeignKey to Borrowing with related_name "payments")
      - session_url, session_id
      - money_to_pay (DecimalField)
    - Prepares database structure for handling payments and fines associated with borrowings

---
+++ payment/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from borrowing.models import Borrowing
+
+
+# Create your models here.
+class Payment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = "PG", "Pending"
+        PAID = "PD", "Paid"
+
+    class Type(models.TextChoices):
+        PAYMENT = "PT", "Payment"
+        FINE = "F", "Fine"
+
+    status = models.CharField(
+        max_length=2,
+        choices=Status
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=Type
+    )
+    borrowing = models.ForeignKey(
+        Borrowing,
+        on_delete=models.CASCADE,
+        related_name="payments"
+    )
+    session_url = models.URLField()
+    session_id = models.TextField()
+    money_to_pay = models.DecimalField(decimal_places=2, max_digits=7)
