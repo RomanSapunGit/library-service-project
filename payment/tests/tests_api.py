c53c01d9445a8c59391f81ce7d4a0a3ab8e0c7cf
RomanSapunGit <sapunroma23@gmail.com>
2025-09-21 20:21:45 +0200

    feat(payment app): add payment app

---
+++ payment/tests/tests_api.py
@@ -0,0 +1,46 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+from rest_framework.test import APIClient
+
+from borrowing.models import Borrowing
+from library.models import Book
+from payment.models import Payment
+
+
+# Create your tests here.
+class PaymentTests(TestCase):
+    def get_url(self, action):
+        return reverse(f"payment:payments-{action}")
+
+    def setUp(self):
+        user = get_user_model()
+        self.user = user.objects.create_user(
+            email="user@user.com", password="pass123"
+        )
+        self.client = APIClient()
+
+        self.book = Book.objects.create(
+            title="Test Book",
+            author="Author",
+            cover="HARD",
+            inventory=2,
+            daily_fee=1.5,
+        )
+
+        self.borrowing = Borrowing.objects.create(
+            user=self.user,
+            book=self.book,
+            borrow_date=timezone.now(),
+            expected_return_date=timezone.now() + timezone.timedelta(days=7),
+        )
+
+        self.payment = Payment.objects.create(
+            status="PG",
+            type="PT",
+            session_url="https://stripe.com",
+            session_id="123",
+            money_to_pay=1234,
+            borrowing=self.borrowing,
+        )
