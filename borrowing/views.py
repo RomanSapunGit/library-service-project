1cda24f453567bc22fcb1976266b9e2cfd201dab
RomanSapunGit <sapunroma23@gmail.com>
2025-09-21 19:01:54 +0200

    feat(borrowing views): override BorrowingView.create to handle book inventory
    Customize create in BorrowingView to:
    - Check if the selected book has available inventory
    - Decrement inventory atomically when creating a borrowing
    - Return 400 Bad Request if inventory is empty

--- borrowing/views.py
+++ borrowing/views.py
@@ -1,12 +1,13 @@
 from django.db import transaction
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.mixins import (
     ListModelMixin,
     CreateModelMixin,
     RetrieveModelMixin
 )
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from borrowing.models import Borrowing
 from borrowing.serializers import (
@@ -14,6 +15,7 @@ from borrowing.serializers import (
     BorrowingDetailSerializer,
     BorrowingReturnSerializer
 )
+from library.models import Book
 
 
 # Create your views here.
@@ -32,6 +34,19 @@ class BorrowingView(
         if self.action == "return_book":
             return BorrowingReturnSerializer
         return BorrowingSerializer
+
+    def create(self, request, *args, **kwargs):
+        book = Book.objects.get(pk=request.data["book"])
+        if book.inventory >= 1:
+            with transaction.atomic():
+                book.inventory -= 1
+                book.save()
+                result = super().create(request, *args, **kwargs)
+                return result
+        return Response(
+            data={"error": "books inventory is empty!"},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
