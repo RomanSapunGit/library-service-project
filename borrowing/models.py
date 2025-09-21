a9ae17910cb5544bcab608ed01e0bdf28cf4521f
RomanSapunGit <sapunroma23@gmail.com>
2025-09-21 18:23:49 +0200

    feat(borrowing models): add Meta constraints to Borrowing model
    Add CheckConstraints in Borrowing.Meta to ensure:
    - expected_return_date is after borrow_date
    - actual_return_date is either null or on/after borrow_date.

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q, F
 
 from library.models import Book
 from user.models import User
@@ -13,3 +14,18 @@ class Borrowing(models.Model):
     actual_return_date = models.DateField(null=True)
     book = models.ForeignKey("library.book", on_delete=models.CASCADE)
     user = models.ForeignKey("user.user", on_delete=models.CASCADE)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=Q(expected_return_date__gt=F("borrow_date")),
+                name="expected_after_borrow",
+            ),
+            models.CheckConstraint(
+                check=(
+                    Q(actual_return_date__isnull=True)
+                    | Q(actual_return_date__gte=F("borrow_date"))
+                ),
+                name="actual_after_borrow",
+            ),
+        ]
