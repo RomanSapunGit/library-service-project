74cf2de31e168e2549b64d4fa6e27b0eb3ea8b08
RomanSapunGit <sapunroma23@gmail.com>
2025-09-22 11:09:44 +0200

    feat(library views): add book inventory counter in perform destroy and create

--- library/views.py
+++ library/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from library.models import Book
 from library.serializers import BookDetailSerializer, BookSerializer
+from utils.redis_utils import delete_book, create_book
 
 
 # Create your views here.
@@ -14,3 +15,10 @@ class BookView(viewsets.ModelViewSet):
         if self.action == "retrieve":
             return BookDetailSerializer
         return BookSerializer
+
+    def create(self, request, *args, **kwargs):
+        create_book(request.data["inventory"])
+        return super().create(request, *args, **kwargs)
+
+    def perform_destroy(self, instance):
+        delete_book(instance.inventory)
